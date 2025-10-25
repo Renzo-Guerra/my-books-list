10e3a506def123eaec82ea3c2f05d3d4bacb5160
Renzo Guerra <renzoguerralatt@gmail.com>
2025-10-25 11:52:15 -0300

    ADD: Delete book modal

--- src/components/desktop/BookRow.tsx
+++ src/components/desktop/BookRow.tsx
@@ -5,6 +5,7 @@ import { FaPenToSquare } from "react-icons/fa6";
 import { useState } from "react";
 import toast from "react-hot-toast";
 import { useBooksContext } from "../../context/BooksProvider";
+import { createPortal } from "react-dom";
 
 export const BookRow = ({ book }: { book: Book }) => {
   const { title, author, state, score } = book;
@@ -23,7 +24,6 @@ export const BookRow = ({ book }: { book: Book }) => {
       console.error(`Error al eliminar el libro: ${title}!`);
       throw error;
     }
-
   }
 
   const handleDelete = async () => {
@@ -37,43 +37,59 @@ export const BookRow = ({ book }: { book: Book }) => {
     ).finally(() => setLayer(false));
   }
 
-
-
   return (
-    <tr className="grid grid-cols-[2fr_1.5fr_1fr_0.5fr_0.9fr] items-center px-4 py-2 border-b border-gray-100 hover:bg-gray-50 transition">
-      <td className="truncate">{title}</td>
-      <td className="truncate">{author}</td>
-      <td className={`text-center font-medium ${colorForState(state)}`}>{state}</td>
-      <td className="text-center">{score ?? "-"}</td>
-      <td>
-        <div className="grid grid-cols-2 gap-2">
-          <button className="flex gap-1 justify-center items-center p-1 cursor-pointer border rounded-sm bg-sky-800 text-white hover:bg-sky-600 transition-colors duration-200">
-            <FaPenToSquare /> Edit
-          </button>
-          <button
-            className="flex gap-1 justify-center items-center p-1 cursor-pointer border rounded-sm bg-red-800 text-white hover:bg-red-600 transition-colors duration-200"
-            onClick={handleDelete}
-          >
-            <FiTrash2 /> Delete
-          </button>
-        </div>
-      </td>
-      {layer && (
-        <div>
-          <p>Are you sure you want to delete <strong>{title}</strong>?</p>
+    <>
+      <tr className="grid grid-cols-[2fr_1.5fr_1fr_0.5fr_0.9fr] items-center px-4 py-2 border-b border-gray-100 hover:bg-gray-50 transition">
+        <td className="truncate">{title}</td>
+        <td className="truncate">{author}</td>
+        <td className={`text-center font-medium ${colorForState(state)}`}>{state}</td>
+        <td className="text-center">{score ?? "-"}</td>
+        <td>
           <div className="grid grid-cols-2 gap-2">
             <button className="flex gap-1 justify-center items-center p-1 cursor-pointer border rounded-sm bg-sky-800 text-white hover:bg-sky-600 transition-colors duration-200">
-              Cancel
+              <FaPenToSquare /> Edit
             </button>
             <button
               className="flex gap-1 justify-center items-center p-1 cursor-pointer border rounded-sm bg-red-800 text-white hover:bg-red-600 transition-colors duration-200"
-              onClick={handleDelete}
+              onClick={() => setLayer(true)}
             >
-              Delete
+              <FiTrash2 /> Delete
             </button>
           </div>
-        </div>
-      )}
-    </tr >
+        </td>
+      </tr >
+
+      {layer &&
+        createPortal(
+          <div
+            className="fixed inset-0 z-50 min-h-auto flex items-center justify-center backdrop-contrast-30 p-4">
+            <div
+              className="flex flex-col bg-white p-6 rounded-xl shadow-2xl max-w-sm w-full"
+            >
+              <h3 className="text-xl font-semibold text-gray-900 m-2">Delete "{book.title}"</h3>
+              <p className="text-gray-600 mb-3">
+                Are you sure you want to delete it?
+              </p>
+
+              <div className="flex justify-end gap-3 mt-4">
+                <button
+                  className="px-4 py-2 rounded-lg text-gray-700 bg-gray-100 hover:bg-gray-200 transition-colors duration-200 font-medium cursor-pointer"
+                  onClick={() => setLayer(false)}
+                >
+                  Cancel
+                </button>
+                <button
+                  className="px-4 py-2 rounded-lg text-white bg-red-600 hover:bg-red-700 transition-colors duration-200 font-medium shadow-sm cursor-pointer"
+                  onClick={handleDelete}
+                >
+                  Delete
+                </button>
+              </div>
+            </div>
+          </div>,
+          document.body
+        )}
+    </>
+
   )
 }
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,71 +1,7 @@
-import type { Book } from "../interfaces";
 import { BookRow } from "../components/desktop/BookRow";
 import { Target } from "../components/mobile";
 import { useBooksContext } from "../context/BooksProvider";
 
-
-const mockBooks: Book[] = [
-  {
-    title: "Don Quijote de la Mancha",
-    author: "Miguel de Cervantes",
-    state: "read",
-    score: 9
-  },
-  {
-    title: "Moby Dick",
-    author: "Herman Melville",
-    state: "pending",
-    score: undefined
-  },
-  {
-    title: "Orgullo y prejuicio",
-    author: "Jane Austen",
-    state: "read",
-    score: 8
-  },
-  {
-    title: "Crimen y castigo",
-    author: "Fiódor Dostoyevski",
-    state: "reading",
-    score: 7
-  },
-  {
-    title: "1984",
-    author: "George Orwell",
-    state: "read",
-    score: 10
-  },
-  {
-    title: "En busca del tiempo perdido",
-    author: "Marcel Proust",
-    state: "reading",
-    score: undefined
-  },
-  {
-    title: "Cien años de soledad",
-    author: "Gabriel García Márquez",
-    state: "read",
-    score: 9
-  },
-  {
-    title: "Los miserables",
-    author: "Victor Hugo",
-    state: "reading",
-    score: undefined
-  },
-  {
-    title: "El retrato de Dorian Gray",
-    author: "Oscar Wilde",
-    state: "read",
-    score: 8
-  },
-  {
-    title: "La Odisea",
-    author: "Homero",
-    state: "pending",
-    score: undefined
-  }
-];
 
 export const HomePage = () => {
   const { books } = useBooksContext();
